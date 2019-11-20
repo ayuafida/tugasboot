330db52c63706a4427afed61d589ea2fe2b4b13b
ayuafida <ayuafidarisarahma18@gmail.com>
2019-11-20 15:02:51 +0700

    Initial commit

---
+++ src/component/Grid.jsx
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+class Grid extends Component {
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta euismod purus,
+                     eu consectetur orci gravida vitae. Phasellus ultricies urna ipsum, placerat dignissim 
+                     elit pellentesque vel.
+                    </div>
+                    <div className="col-md-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta euismod purus,
+                     eu consectetur orci gravida vitae. Phasellus ultricies urna ipsum, placerat dignissim 
+                     elit pellentesque vel.
+                    </div>
+                    <div className="col-md-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta euismod purus,
+                     eu consectetur orci gravida vitae. Phasellus ultricies urna ipsum, placerat dignissim 
+                     elit pellentesque vel.
+                    </div>
+                    <div className="col-md-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta euismod purus,
+                     eu consectetur orci gravida vitae. Phasellus ultricies urna ipsum, placerat dignissim 
+                     elit pellentesque vel.
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Grid;
